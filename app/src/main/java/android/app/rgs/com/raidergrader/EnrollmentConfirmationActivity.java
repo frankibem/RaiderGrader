00e28dbf7580b28de2fb801a0e880127321d6c0f
ljoness <lauren.joness@gmail.com>
2015-10-04 18:09:17 -0500

    Enrollment branch

--- app/src/main/java/android/app/rgs/com/raidergrader/EnrollmentConfirmationActivity.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/EnrollmentConfirmationActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class EnrollmentConfirmationActivity extends AppCompatActivity {
 
     TextView outputEdit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,11 @@ public class EnrollmentConfirmationActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String outputText = String.format("Class ID: %s", intent.getStringExtra("classid"));
         outputEdit.setText(outputText);
+    }
+
+    public void onClickConfirm(View v) {
+
+
     }
 
     @Override
