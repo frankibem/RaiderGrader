c31f7f5d7466bdd4412e942dab4eed5a96bb7248
ljoness <lauren.joness@gmail.com>
2015-10-04 16:52:41 -0500

    Enrollment branch

---
+++ app/src/main/java/android/app/rgs/com/raidergrader/EnrollmentConfirmationActivity.java
@@ -0,0 +1,45 @@
+package android.app.rgs.com.raidergrader;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class EnrollmentConfirmationActivity extends AppCompatActivity {
+
+    TextView outputEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enrollment_confirmation);
+        outputEdit = (TextView) findViewById(R.id.enrollconfirm);
+        Intent intent = getIntent();
+        String outputText = String.format("Class ID: %s", intent.getStringExtra("classid"));
+        outputEdit.setText(outputText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_enrollment_confirmation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/android/app/rgs/com/raidergrader/activities/EnrollmentActivity.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/activities/EnrollmentActivity.java
@@ -1,30 +1,40 @@
 package android.app.rgs.com.raidergrader.activities;
 
+import android.app.rgs.com.raidergrader.EnrollmentConfirmationActivity;
 import android.app.rgs.com.raidergrader.R;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EnrollmentActivity extends AppCompatActivity {
+
+    Button submitBtn;
+    EditText classIDEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enrollment);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        submitBtn = (Button) findViewById(R.id.submit_btn);
+        classIDEdit = (EditText) findViewById(R.id.classID_text);
+
+        setSupportActionBar(toolbar);
+    }
+
+    public void onClickSubmit(View v) {
+        String classid = classIDEdit.getText().toString();
+        Intent intent = new Intent(this, EnrollmentConfirmationActivity.class);
+        intent.putExtra("classid", classid);
+        startActivity(intent);
+
     }
 
 }
