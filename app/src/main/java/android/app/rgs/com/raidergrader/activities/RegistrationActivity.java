b43b5bbb122786f53e968210a058dbb67fb8185d
sungyuri23 <claire.ancajas@ttu.edu>
2015-10-06 11:18:31 -0500

    updated registration layout

--- app/src/main/java/android/app/rgs/com/raidergrader/activities/RegistrationActivity.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/activities/RegistrationActivity.java
@@ -20,18 +20,6 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        submit1 = (Button)findViewById(R.id.submittxt);
-
     }
 
     public void onClick(View v) {
