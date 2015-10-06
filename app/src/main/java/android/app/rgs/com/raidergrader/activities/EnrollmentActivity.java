6f190010366aeaed224821bac9abc4c789f9afc3
ljoness <lauren.joness@gmail.com>
2015-10-06 16:17:18 -0500

    Added skeleton for network code

--- app/src/main/java/android/app/rgs/com/raidergrader/activities/EnrollmentActivity.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/activities/EnrollmentActivity.java
@@ -1,37 +1,59 @@
 package android.app.rgs.com.raidergrader.activities;
 
+import android.app.ProgressDialog;
 import android.app.rgs.com.raidergrader.R;
+import android.app.rgs.com.raidergrader.data_access.RestTask;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class EnrollmentActivity extends AppCompatActivity {
+public class EnrollmentActivity extends AppCompatActivity
+implements RestTask.ProgressCallback, RestTask.ResponseCallback{
 
     Button submitBtn;
-    EditText classIDEdit;
+    EditText inputClassId;
+    TextInputLayout inputLayoutClassId;
+    ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enrollment);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         submitBtn = (Button) findViewById(R.id.submit_btn);
-        classIDEdit = (EditText) findViewById(R.id.classID_text);
+        inputClassId = (EditText) findViewById(R.id.input_classid);
+        inputLayoutClassId = (TextInputLayout) findViewById(R.id.input_layout_classid);
 
-        setSupportActionBar(toolbar);
+//        inputClassId.addTextChangedListener();
     }
 
     public void onClickSubmit(View v) {
-        String classid = classIDEdit.getText().toString();
+        String classid = inputClassId.getText().toString();
         Intent intent = new Intent(this, EnrollmentConfirmationActivity.class);
         intent.putExtra("classid", classid);
         startActivity(intent);
 
     }
 
+    @Override
+    public void onProgressUpdate(int progress) {
+
+    }
+
+    @Override
+    public void onRequestSuccess(String response) {
+
+    }
+
+    @Override
+    public void onRequestError(Exception error) {
+
+    }
 }
