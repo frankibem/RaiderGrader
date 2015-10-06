f7a230126129f41492e392412b4372fdfbca6b4a
sungyuri23 <claire.ancajas@ttu.edu>
2015-10-06 11:26:06 -0500

    added register class

---
+++ app/src/main/java/android/app/rgs/com/raidergrader/models/RegisterModel.java
@@ -0,0 +1,9 @@
+package android.app.rgs.com.raidergrader.models;
+
+public class RegisterModel {
+    public String FirstName;
+    public String LastName;
+    public String Email;
+    public String Password;
+    public String Role;
+}
