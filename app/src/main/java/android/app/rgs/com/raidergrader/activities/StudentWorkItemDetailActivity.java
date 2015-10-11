90d183ea0995696cec24abb102376a68f121523f
Frank Ibem <soulsolja@yahoo.com>
2015-10-11 15:49:23 -0500

    Added activities for iteration 3

---
+++ app/src/main/java/android/app/rgs/com/raidergrader/activities/StudentWorkItemDetailActivity.java
@@ -0,0 +1,14 @@
+package android.app.rgs.com.raidergrader.activities;
+
+import android.app.rgs.com.raidergrader.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentWorkItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_work_item_detail);
+    }
+}
