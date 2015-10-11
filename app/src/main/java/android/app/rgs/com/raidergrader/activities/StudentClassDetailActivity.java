bb35ff4fd85452232649032580d2e3425ecbeb8b
Frank Ibem <soulsolja@yahoo.com>
2015-10-11 16:05:32 -0500

    Updated activity selector page

---
+++ app/src/main/java/android/app/rgs/com/raidergrader/activities/StudentClassDetailActivity.java
@@ -0,0 +1,14 @@
+package android.app.rgs.com.raidergrader.activities;
+
+import android.app.rgs.com.raidergrader.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentClassDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_class_detail);
+    }
+}
