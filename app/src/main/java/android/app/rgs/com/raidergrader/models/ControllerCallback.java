c0867cbb9776f877fe679d4f5d1d93dc539a7644
Frank Ibem <soulsolja@yahoo.com>
2015-10-16 11:54:32 -0500

    Added ControllerCallback interface

---
+++ app/src/main/java/android/app/rgs/com/raidergrader/models/ControllerCallback.java
@@ -0,0 +1,8 @@
+package android.app.rgs.com.raidergrader.models;
+
+/**
+ * Created by Frank Ibem on 10/16/2015.
+ */
+public interface ControllerCallback {
+    void DisplayResult(Object result);
+}
