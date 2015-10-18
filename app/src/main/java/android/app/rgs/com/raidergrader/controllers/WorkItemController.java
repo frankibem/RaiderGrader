45830fb3ae6f27fb17296cf7839de14657c6aafe
norogers <noris.g.rogers@ttu.edu>
2015-10-18 15:26:28 -0500

    Created UpdateWorkItem function

--- app/src/main/java/android/app/rgs/com/raidergrader/controllers/WorkItemController.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/controllers/WorkItemController.java
@@ -2,7 +2,22 @@ package android.app.rgs.com.raidergrader.controllers;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.app.rgs.com.raidergrader.activities.StudentClassListActivity;
+import android.app.rgs.com.raidergrader.activities.StudentWorkItemDetailActivity;
+import android.app.rgs.com.raidergrader.data_access.HttpStatusCodes;
+import android.app.rgs.com.raidergrader.data_access.Repository;
+import android.app.rgs.com.raidergrader.data_access.RequestError;
+import android.app.rgs.com.raidergrader.data_access.RestTask;
+import android.app.rgs.com.raidergrader.data_access.RestUtil;
+import android.app.rgs.com.raidergrader.helpers.GlobalHandling;
+import android.app.rgs.com.raidergrader.helpers.JsonHelpers;
 import android.app.rgs.com.raidergrader.models.ControllerCallback;
+import android.app.rgs.com.raidergrader.models.UpdateWorkItemModel;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * Created by Frank Ibem on 10/16/2015.
@@ -21,5 +36,52 @@ public class WorkItemController {
     public WorkItemController(Activity activity, ControllerCallback callback) {
         this.activity = activity;
         this.controllerCallback = callback;
+    }
+
+    /**
+     * Noris Rogers
+     * Updates the details of a work item
+     *
+     */
+    public void UpdateWorkItem(UpdateWorkItemModel updateWorkItemModel)
+    {
+        Gson gson = JsonHelpers.getGson();
+        String request = gson.toJson(updateWorkItemModel);
+
+        RestTask.ResponseCallback responseCallback = new RestTask.ResponseCallback() {
+            @Override
+            public void onRequestSuccess(String response) {
+                if (mProgress != null) {
+                    mProgress.dismiss();
+                }
+                Intent intent = new Intent(activity, StudentWorkItemDetailActivity.class);
+                activity.startActivity(intent);
+                Toast.makeText(activity, "Work Item Updated", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onRequestError(RequestError error) {
+                GlobalHandling.generalError(activity, error);
+            }
+        };
+
+        RestTask.ProgressCallback progressCallback = new RestTask.ProgressCallback() {
+            @Override
+            public void onProgressUpdate(int progress) {
+            }
+        };
+
+        try {
+            RestTask task = RestUtil.obtainGetTask(Repository.baseUrl + "api/WorkItems");
+            task.setProgressCallback(progressCallback);
+            task.setResponseCallback(responseCallback);
+            task.execute();
+
+            mProgress = ProgressDialog.show(activity, "Loading", "Updating Your Work Item", true);
+        } catch (Exception ex) {
+            responseCallback.onRequestError(new RequestError(HttpStatusCodes.Incomplete, ex.getMessage()));
+        }
+
     }
 }
