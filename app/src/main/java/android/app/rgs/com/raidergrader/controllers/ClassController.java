3290d227e3cbbf54f99d490cd0a9aefc021cf500
michael-arroyo <michael.arroyo@ttu.edu>
2015-10-18 15:13:14 -0500

    Added UpdateClass method.

--- app/src/main/java/android/app/rgs/com/raidergrader/controllers/ClassController.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/controllers/ClassController.java
@@ -2,7 +2,18 @@ package android.app.rgs.com.raidergrader.controllers;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.app.rgs.com.raidergrader.data_access.HttpStatusCodes;
+import android.app.rgs.com.raidergrader.data_access.Repository;
+import android.app.rgs.com.raidergrader.data_access.RequestError;
+import android.app.rgs.com.raidergrader.data_access.RestTask;
+import android.app.rgs.com.raidergrader.data_access.RestUtil;
+import android.app.rgs.com.raidergrader.helpers.GlobalHandling;
+import android.app.rgs.com.raidergrader.models.ClassModel;
 import android.app.rgs.com.raidergrader.models.ControllerCallback;
+import android.app.rgs.com.raidergrader.models.UpdateClassModel;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
 
 /**
  * Created by Frank Ibem on 10/16/2015.
@@ -22,4 +33,60 @@ public class ClassController {
         this.activity = activity;
         this.controllerCallback = callback;
     }
+
+    /**
+     * Update the details of a class
+     * @param model
+     */
+    public void UpdateClass(UpdateClassModel model){
+        Gson gson = new Gson();
+        String request = gson.toJson(model);
+
+        RestTask.ResponseCallback responseCallback = new RestTask.ResponseCallback() {
+
+            public void onRequestSuccess(String response) {
+                if (mProgress != null){
+                    mProgress.dismiss();
+                }
+
+                Toast.makeText(activity, "Class updated.", Toast.LENGTH_SHORT).show();
+
+                controllerCallback.DisplayResult(null);
+            }
+
+            @Override
+            public void onRequestError(RequestError error) {
+                if (mProgress != null){
+                    mProgress.dismiss();
+                }
+
+                //TODO: Add appropriate error handling later
+                if(mProgress != null) {
+                }
+                if (error.getStatusCode() == HttpStatusCodes.BadRequest) {
+                    GlobalHandling.makeShortToast(activity, "Please review your input.");
+                } else {
+                    GlobalHandling.generalError(activity, error);
+                }
+            }
+        };
+
+        RestTask.ProgressCallback progressCallback = new RestTask.ProgressCallback() {
+            @Override
+            public void onProgressUpdate(int progress) {
+            }
+        };
+
+        try{
+            RestTask task = RestUtil.obtainJSONPutTask(Repository.baseUrl + "api/classes", request);
+            task.setProgressCallback(progressCallback);
+            task.setResponseCallback(responseCallback);
+            task.execute();
+
+            mProgress = ProgressDialog.show(activity, "Loading", "Updating class details", true);
+        } catch (Exception ex) {
+            responseCallback.onRequestError(new RequestError(HttpStatusCodes.Incomplete, ex.getMessage()));
+        }
+    }
 }
+
