a539ae31f26654c8460700f13bc80a669cfa0e0b
jhernandez8 <joshua.hernandez@ttu.edu>
2015-10-18 14:48:58 -0500

    Created Class code (Pushing this to the branch) Done by ME

--- app/src/main/java/android/app/rgs/com/raidergrader/controllers/ClassController.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/controllers/ClassController.java
@@ -2,7 +2,20 @@ package android.app.rgs.com.raidergrader.controllers;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.app.rgs.com.raidergrader.data_access.HttpStatusCodes;
+import android.app.rgs.com.raidergrader.data_access.Repository;
+import android.app.rgs.com.raidergrader.data_access.RequestError;
+import android.app.rgs.com.raidergrader.data_access.RestTask;
+import android.app.rgs.com.raidergrader.data_access.RestUtil;
+import android.app.rgs.com.raidergrader.helpers.GlobalHandling;
+import android.app.rgs.com.raidergrader.helpers.JsonHelpers;
 import android.app.rgs.com.raidergrader.models.ControllerCallback;
+import android.app.rgs.com.raidergrader.models.CreateClassModel;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
 
 /**
  * Created by Frank Ibem on 10/16/2015.
@@ -21,5 +34,58 @@ public class ClassController {
     public ClassController(Activity activity, ControllerCallback callback) {
         this.activity = activity;
         this.controllerCallback = callback;
+    }
+
+    /**
+     * Creates a new class for a teacher from the given model
+     *
+     * @param createClassModel
+     */
+    public void CreateClass(CreateClassModel createClassModel) {
+        Gson gson = JsonHelpers.getGson();
+        String request = gson.toJson(createClassModel);
+
+        RestTask.ResponseCallback responseCallback = new RestTask.ResponseCallback() {
+            @Override
+            public void onRequestSuccess(String response) {
+                if (mProgress != null) {
+                    mProgress.dismiss();
+                }
+
+                Toast.makeText(activity, "Class successfully created", Toast.LENGTH_SHORT).show();
+                controllerCallback.DisplayResult(null);
+            }
+
+            @Override
+            public void onRequestError(RequestError error) {
+                if (mProgress != null) {
+                    mProgress.dismiss();
+                }
+
+                // TODO: Add appropriate error handling later
+                if (error.getStatusCode() == HttpStatusCodes.BadRequest) {
+                    GlobalHandling.makeShortToast(activity, "Please review your input");
+                } else {
+                    GlobalHandling.generalError(activity, error);
+                }
+            }
+        };
+
+        RestTask.ProgressCallback progressCallback = new RestTask.ProgressCallback() {
+            @Override
+            public void onProgressUpdate(int progress) {
+            }
+        };
+
+        try {
+            RestTask task = RestUtil.obtainJSONPostTask(Repository.baseUrl+"api/Classes", request);
+            task.setResponseCallback(responseCallback);
+            task.setProgressCallback(progressCallback);
+            task.execute();
+
+            mProgress = ProgressDialog.show(activity, "Loading", "Creating your class", true);
+        } catch (Exception ex) {
+            responseCallback.onRequestError(new RequestError(HttpStatusCodes.Incomplete, ex.getMessage()));
+        }
     }
 }
