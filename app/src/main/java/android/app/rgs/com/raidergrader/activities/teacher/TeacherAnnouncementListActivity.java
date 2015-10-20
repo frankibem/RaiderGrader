8b09f1587c56e1f8b00e4b086e3dcbc9e9ef71b5
Frank Ibem <soulsolja@yahoo.com>
2015-10-20 13:18:48 -0500

    Added teacher activities

---
+++ app/src/main/java/android/app/rgs/com/raidergrader/activities/teacher/TeacherAnnouncementListActivity.java
@@ -0,0 +1,14 @@
+package android.app.rgs.com.raidergrader.activities.teacher;
+
+import android.app.rgs.com.raidergrader.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherAnnouncementListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teacher_announcement_list);
+    }
+}
