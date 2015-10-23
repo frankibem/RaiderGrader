90b1327f0287f2ed8523e509e31f4f120dfa9987
Frank Ibem <soulsolja@yahoo.com>
2015-10-23 07:58:27 -0500

    Added date and time picker dialogs as well and corresponding interfaces

---
+++ app/src/main/java/android/app/rgs/com/raidergrader/dialogs/DatePickerFragment.java
@@ -0,0 +1,56 @@
+package android.app.rgs.com.raidergrader.dialogs;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.app.rgs.com.raidergrader.models.OnDatePickedListener;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+/**
+ * Created by Frank Ibem on 10/22/2015.
+ */
+
+/**
+ * Fragment that allows the user to select a Date
+ */
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+    private int mYear, mMonth, mDay;
+    private OnDatePickedListener dateListener;
+
+    /**
+     * Sets the start date for the DatePickerDialog
+     *
+     * @param year  The year
+     * @param month The month (1 ~ 12)
+     * @param day   The day of the month
+     */
+    public void setStartDate(int year, int month, int day) {
+        mYear = year;
+        mMonth = month;
+        mDay = day;
+    }
+
+    /**
+     * Sets the listener that should be called after the user has picked a date
+     *
+     * @param dateListener The listener to listen for the DateSet event
+     */
+    public void setDateListener(OnDatePickedListener dateListener) {
+        this.dateListener = dateListener;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new DatePickerDialog(getActivity(), this, mYear, mMonth, mDay);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        if (dateListener != null) {
+            dateListener.onDatePicked(year, monthOfYear, dayOfMonth);
+        }
+    }
+}
---
+++ app/src/main/java/android/app/rgs/com/raidergrader/models/OnDatePickedListener.java
@@ -0,0 +1,8 @@
+package android.app.rgs.com.raidergrader.models;
+
+/**
+ * Created by Frank Ibem on 10/22/2015.
+ */
+public interface OnDatePickedListener {
+    void onDatePicked(int year, int month, int dayOfMonth);
+}
