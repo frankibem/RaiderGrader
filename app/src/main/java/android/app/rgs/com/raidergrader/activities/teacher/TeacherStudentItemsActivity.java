366f6ef3f2c3bd2e759afa1a4cc443e17eeda031
norogers <noris.g.rogers@ttu.edu>
2015-10-25 16:22:43 -0500

    Added Teacher Student Item activity

--- app/src/main/java/android/app/rgs/com/raidergrader/activities/teacher/TeacherStudentItemsActivity.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/activities/teacher/TeacherStudentItemsActivity.java
@@ -1,14 +1,44 @@
 package android.app.rgs.com.raidergrader.activities.teacher;
 
 import android.app.rgs.com.raidergrader.R;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TeacherStudentItemsActivity extends AppCompatActivity {
+
+    ListView listView;
+    private String[] studentItems = {"Current", "Wait list"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teacher_student_items);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, studentItems);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent intent0 = new Intent(getApplicationContext(), TeacherClassAcceptedStudentsActivity.class);
+                        break;
+                    case 1:
+                        Intent intent1 = new Intent(getApplicationContext(), TeacherClassWaitlistActivity.class);
+                        break;
+                }
+
+            }
+
+
+        });
     }
 }
