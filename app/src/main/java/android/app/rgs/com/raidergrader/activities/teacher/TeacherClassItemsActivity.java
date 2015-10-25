0886d1b31a1696c63db2050858d4d2879e9bdb8f
jhernandez8 <joshua.hernandez@ttu.edu>
2015-10-25 17:03:35 -0500

    Implemented TeacherClassItemsActivity

--- app/src/main/java/android/app/rgs/com/raidergrader/activities/teacher/TeacherClassItemsActivity.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/activities/teacher/TeacherClassItemsActivity.java
@@ -1,14 +1,49 @@
 package android.app.rgs.com.raidergrader.activities.teacher;
 
+/**
+ * @author Joshua Hernandez
+ */
+
 import android.app.rgs.com.raidergrader.R;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TeacherClassItemsActivity extends AppCompatActivity {
+    String[] data = {"Students", "Work items", "Announcements"};
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teacher_class_items);
+
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent intent = new Intent(getApplicationContext(), TeacherStudentItemsActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        Intent intent1 = new Intent(getApplicationContext(), TeacherWorkItemListActivity.class);
+                        startActivity(intent1);
+                        break;
+                    case 2:
+                        Intent intent2 = new Intent(getApplicationContext(), TeacherAnnouncementListActivity.class);
+                        startActivity(intent2);
+                        break;
+                }
+            }
+        });
     }
 }
