77df23b37cc30b935eda43722c504c0d97f733f1
ljoness <lauren.joness@gmail.com>
2015-10-26 12:51:26 -0500

    Implemented StudentClassItemsActivity

--- app/src/main/java/android/app/rgs/com/raidergrader/activities/student/StudentClassItemsActivity.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/activities/student/StudentClassItemsActivity.java
@@ -2,16 +2,49 @@ package android.app.rgs.com.raidergrader.activities.student;
 
 import android.app.rgs.com.raidergrader.R;
 import android.app.rgs.com.raidergrader.controllers.AccountController;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class StudentClassItemsActivity extends AppCompatActivity {
+    private static final String WORKITEMS = "Work items";
+    private static final String ANNOUNCEMENTS = "Announcements";
+    private static String[] data = {WORKITEMS, ANNOUNCEMENTS};
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_class_items);
+
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = data[position];
+                switch (item) {
+                    case WORKITEMS:
+                        Intent intentWorkItems = new Intent(getApplicationContext(), StudentWorkItemListActivity.class);
+                        startActivity(intentWorkItems);
+                        break;
+                    case ANNOUNCEMENTS:
+                        Intent intentAnnouncement = new Intent(getApplicationContext(), StudentAnnouncementListActivity.class);
+                        startActivity(intentAnnouncement);
+                        break;
+                    default:
+                        return;
+                }
+            }
+        });
     }
 
     @Override
