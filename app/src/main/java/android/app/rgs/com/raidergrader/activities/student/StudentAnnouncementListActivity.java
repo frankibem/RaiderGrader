d55f10e81118d5da1a9d1f4c55a994f83bed9f08
michael-arroyo <michael.arroyo@ttu.edu>
2015-10-27 18:26:57 -0500

    Completed StudentAnnouncementListActivity

--- app/src/main/java/android/app/rgs/com/raidergrader/activities/student/StudentAnnouncementListActivity.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/activities/student/StudentAnnouncementListActivity.java
@@ -1,27 +1,42 @@
 package android.app.rgs.com.raidergrader.activities.student;
 
 import android.app.rgs.com.raidergrader.R;
+import android.app.rgs.com.raidergrader.adapters.StudentAnnouncementListAdapter;
 import android.app.rgs.com.raidergrader.controllers.AccountController;
+import android.app.rgs.com.raidergrader.controllers.AnnouncementController;
+import android.app.rgs.com.raidergrader.data_access.Repository;
+import android.app.rgs.com.raidergrader.models.AnnouncementModel;
+import android.app.rgs.com.raidergrader.models.ControllerCallback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class StudentAnnouncementListActivity extends AppCompatActivity {
-
-
+public class StudentAnnouncementListActivity extends AppCompatActivity
+        implements ControllerCallback<List<AnnouncementModel>> {
     private ListView listView;
+    private StudentAnnouncementListActivity activity;
+
+    private AnnouncementController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_announcement_list);
+        activity = this;
 
         listView = (ListView) findViewById(R.id.listView);
+        controller = new AnnouncementController(this, this);
+        fetchData();
+    }
+
+    private void fetchData(){
+        controller.GetAnnouncementsforClass(Repository.getCurrentClass().Id);
     }
 
     @Override
@@ -44,4 +59,9 @@ public class StudentAnnouncementListActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void DisplayResult(List<AnnouncementModel> result) {
+        StudentAnnouncementListAdapter adapter = new StudentAnnouncementListAdapter(this, result);
+        listView.setAdapter(adapter);
+    }
 }
