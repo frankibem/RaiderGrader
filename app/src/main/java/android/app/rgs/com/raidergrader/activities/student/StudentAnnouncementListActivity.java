2d39dca21b88545984d6a9948e649468bfc96bd3
michael-arroyo <michael.arroyo@ttu.edu>
2015-10-27 18:35:45 -0500

    Functionality: Navigate to Announcement Detail

--- app/src/main/java/android/app/rgs/com/raidergrader/activities/student/StudentAnnouncementListActivity.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/activities/student/StudentAnnouncementListActivity.java
@@ -7,10 +7,12 @@ import android.app.rgs.com.raidergrader.controllers.AnnouncementController;
 import android.app.rgs.com.raidergrader.data_access.Repository;
 import android.app.rgs.com.raidergrader.models.AnnouncementModel;
 import android.app.rgs.com.raidergrader.models.ControllerCallback;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -21,6 +23,7 @@ public class StudentAnnouncementListActivity extends AppCompatActivity
         implements ControllerCallback<List<AnnouncementModel>> {
     private ListView listView;
     private StudentAnnouncementListActivity activity;
+    private List<AnnouncementModel> announcements;
 
     private AnnouncementController controller;
 
@@ -33,9 +36,20 @@ public class StudentAnnouncementListActivity extends AppCompatActivity
         listView = (ListView) findViewById(R.id.listView);
         controller = new AnnouncementController(this, this);
         fetchData();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AnnouncementModel announce = announcements.get(position);
+                Repository.setCurrentAnnouncement(announce);
+
+                Intent intent = new Intent(activity, StudentAnnouncementDetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    private void fetchData(){
+    private void fetchData() {
         controller.GetAnnouncementsforClass(Repository.getCurrentClass().Id);
     }
 
@@ -61,6 +75,8 @@ public class StudentAnnouncementListActivity extends AppCompatActivity
 
     @Override
     public void DisplayResult(List<AnnouncementModel> result) {
+        announcements = result;
+
         StudentAnnouncementListAdapter adapter = new StudentAnnouncementListAdapter(this, result);
         listView.setAdapter(adapter);
     }
