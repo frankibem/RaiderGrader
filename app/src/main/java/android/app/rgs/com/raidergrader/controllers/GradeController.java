e70a3288be0e85b62f085b19aab46a3c424c8cf1
Frank Ibem <soulsolja@yahoo.com>
2015-10-30 11:13:38 -0500

    Move UpdateScoresForWorkItem to GradeController fro WorkItemController

--- app/src/main/java/android/app/rgs/com/raidergrader/controllers/GradeController.java
+++ app/src/main/java/android/app/rgs/com/raidergrader/controllers/GradeController.java
@@ -7,10 +7,12 @@ import android.app.rgs.com.raidergrader.data_access.Repository;
 import android.app.rgs.com.raidergrader.data_access.RequestError;
 import android.app.rgs.com.raidergrader.data_access.RestTask;
 import android.app.rgs.com.raidergrader.data_access.RestUtil;
+import android.app.rgs.com.raidergrader.models.UpdateWorkItemModel;
 import android.app.rgs.com.raidergrader.utilities.GlobalHandling;
 import android.app.rgs.com.raidergrader.utilities.JsonHelpers;
 import android.app.rgs.com.raidergrader.models.ControllerCallback;
 import android.app.rgs.com.raidergrader.models.ScoreUnitModel;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -87,6 +89,59 @@ public class GradeController {
             task.execute();
 
             mProgress = ProgressDialog.show(activity, "Loading", "Creating your account", true);
+        } catch (Exception ex) {
+            responseCallback.onRequestError(new RequestError(HttpStatusCodes.Incomplete, ex.getMessage()));
+        }
+    }
+
+    /**
+     * Update the grades of students for a particular work item
+     * Created by Michael Arroyo on 10/18/2015
+     * @param model
+     */
+    public void UpdateScoresForWorkItem(List<UpdateWorkItemModel> model) {
+        Gson gson = JsonHelpers.getGson();
+        String request = gson.toJson(model);
+
+        RestTask.ResponseCallback responseCallback = new RestTask.ResponseCallback() {
+            @Override
+            public void onRequestSuccess(String response) {
+                if (mProgress != null) {
+                    mProgress.dismiss();
+                }
+
+                Toast.makeText(activity, "Grades updated.", Toast.LENGTH_SHORT).show();
+                controllerCallback.DisplayResult(null);
+            }
+
+            @Override
+            public void onRequestError(RequestError error) {
+                if (mProgress != null) {
+                    mProgress.dismiss();
+                }
+
+                //TODO: Add appropriate error handling later
+                if (error.getStatusCode() == HttpStatusCodes.BadRequest) {
+                    GlobalHandling.makeShortToast(activity, "Please review your input.");
+                } else {
+                    GlobalHandling.generalError(activity, error);
+                }
+            }
+        };
+
+        RestTask.ProgressCallback progressCallback = new RestTask.ProgressCallback() {
+            @Override
+            public void onProgressUpdate(int progress) {
+            }
+        };
+
+        try {
+            RestTask task = RestUtil.obtainJSONPutTask(Repository.baseUrl + "api/ScoreUnits", request);
+            task.setProgressCallback(progressCallback);
+            task.setResponseCallback(responseCallback);
+            task.execute();
+
+            mProgress = ProgressDialog.show(activity, "Loading", "Updating grades", true);
         } catch (Exception ex) {
             responseCallback.onRequestError(new RequestError(HttpStatusCodes.Incomplete, ex.getMessage()));
         }
